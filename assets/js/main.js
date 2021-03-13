c22472e2e6a77b8946a5a10f6130f656b6059e5f
muhuahaha <supercodekim@gmail.com>
2021-03-13 21:57:15 +0100

    test

---
+++ assets/js/main.js
@@ -0,0 +1,85 @@
+const points = (twoPointers, threePointers) => twoPointers * 2 + threePointers * 3;
+points();
+
+const testData = `Test.assertEquals(points(1, 1), 5)
+Test.assertEquals(points(1, 2), 8)
+Test.assertEquals(points(2, 1), 7)
+Test.assertEquals(points(2, 2), 10)
+Test.assertEquals(points(69, 420), 1398)`;
+
+const funk = testData.match(/(?<=\().*(?=,)/g);
+const ergebnis = testData.match(/(?<=\), ).*(?=\))/g);
+
+console.log(funk);
+// const ergebnis = testData.match(/(?<=\), ").*(?="\)))/g);  mit String
+
+function consolo() {
+  return funk.map((x, i) => `const consolo${i + 1} = ${x}`).join('; <br>');
+}
+
+function displayErgebnis() {
+  return ergebnis.map((x, i) => `const ergebnis${i + 1} = ${x}`).join('; <br>');
+}
+
+console.log(consolo());
+console.log(displayErgebnis());
+
+const resultFunk = consolo();
+const resultErgebnis = displayErgebnis();
+
+// Make a div
+const div = document.createElement('div');
+// add a class of wrapper to it
+div.classList.add('wrapper');
+
+// put it into the body
+document.body.appendChild(div);
+
+// make an unordered list
+const h2Test = `<h2>
+  ${funk[0]}
+</h2><p>${resultFunk} <br><br> ${resultErgebnis}</p>`;
+
+// add three list items with the words "one, two three" in them
+// put that list into the above wrapper
+div.innerHTML = h2Test;
+
+function generatePlayerCard(funki, ergebnisse, numm, fun) {
+  const farbeTrue = `${funki.toString()}` === ergebnisse;
+
+  const html = `
+      <div class="playerCard">
+        <h2>${funki}</h2>
+        <p>${ergebnisse}<p>
+        <p>Test ${numm} ist ${farbeTrue}.</p>
+        <p>${fun}</p>
+        <div><button class="delete" type="button">&times; Delete</button></div>
+      </div>
+    `;
+
+  return html;
+}
+
+const cards = document.createElement('div');
+cards.classList.add('cards');
+
+let cardsHTML = [];
+for (let i = 0; i < funk.length; i++) {
+  cardsHTML += generatePlayerCard(eval(funk[i]), ergebnis[i], i + 1, funk[i]);
+}
+
+cards.innerHTML = cardsHTML;
+div.insertAdjacentElement('beforebegin', cards);
+
+const buttonsDelete = document.querySelectorAll('.delete');
+// make out delete function
+function deleteCard(event) {
+  const buttonThatGotClicked = event.currentTarget;
+  // buttonThatGotClicked.parentElement.remove();
+  buttonThatGotClicked.closest('.playerCard').remove();
+}
+
+// loop over them and attach a listener
+buttonsDelete.forEach(button => button.addEventListener('click', deleteCard));
+
+// console.log(document.getElementsByClassName('playerCard'));
