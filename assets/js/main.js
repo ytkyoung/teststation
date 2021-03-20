6081085f97b08e16900e90d2933d72a9cb9028e7
muhuahaha <kimyou2019@gmail.com>
2021-03-20 04:53:18 +0100

    evenLast

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,31 @@
-function getMissingLetters(s) {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-	return [...alphabet].filter(x => !s.match(x)).join('');
-}
+const evenLast = arr =>
+  arr.length ? arr.map((x, i) => (!(i % 2) ? x : 0)).reduce((a, b) => a + b) * arr[arr.length - 1] : 0;
 
-console.log(getMissingLetters);
+const result = evenLast([1, 4, 5, 6, 7, 2, 3]);
+console.log(result);
 
-const testData = `Test.assertEquals(getMissingLetters("abcdefgpqrstuvwxyz"), "hijklmno")
-Test.assertEquals(getMissingLetters("zyxwvutsrq"), "abcdefghijklmnop")
-Test.assertEquals(getMissingLetters(""), "abcdefghijklmnopqrstuvwxyz")
-Test.assertEquals(getMissingLetters("abcdefghijklmnopqrstuvwxyz"), "")
-Test.assertEquals(getMissingLetters("qinjwm"), "abcdefghkloprstuvxyz")
-Test.assertEquals(getMissingLetters("luiqtkgwzheapr"), "bcdfjmnosvxy")
-Test.assertEquals(getMissingLetters("qankj"), "bcdefghilmoprstuvwxyz")
-Test.assertEquals(getMissingLetters("bawgeuskmfcrpodnxztviy"), "hjlq")
-Test.assertEquals(getMissingLetters("gdpna"), "bcefhijklmoqrstuvwxyz")
-Test.assertEquals(getMissingLetters("hbi"), "acdefgjklmnopqrstuvwxyz")
-Test.assertEquals(getMissingLetters("djxifockvwhaqbnmstzpylu"), "egr")
-Test.assertEquals(getMissingLetters("gastzwjcnvoprfmxd"), "behiklquy")
-Test.assertEquals(getMissingLetters("xuti"), "abcdefghjklmnopqrsvwyz")
-Test.assertEquals(getMissingLetters("abc"), "defghijklmnopqrstuvwxyz")
-Test.assertEquals(getMissingLetters("kchiatvmbqrdsyopwju"), "efglnxz")
-Test.assertEquals(getMissingLetters("xbkj"), "acdefghilmnopqrstuvwyz")
-Test.assertEquals(getMissingLetters("cbvkyroes"), "adfghijlmnpqtuwxz")
-Test.assertEquals(getMissingLetters("s"), "abcdefghijklmnopqrtuvwxyz")
-Test.assertEquals(getMissingLetters("idvgzujmswloefcthb"), "aknpqrxy")
-Test.assertEquals(getMissingLetters("fmoraygevhkxzutcjd"), "bilnpqsw")
-Test.assertEquals(getMissingLetters("vuynqpaf"), "bcdeghijklmorstwxz")
-Test.assertEquals(getMissingLetters("j"), "abcdefghiklmnopqrstuvwxyz")`;
+const testData = `Test.assertEquals(evenLast([]), 0)
+Test.assertEquals(evenLast([1, 3, 3, 1, 10]), 140)
+Test.assertEquals(evenLast([-11, 3, 3, 1, 10]), 20)
+Test.assertEquals(evenLast([1, 31, 3, 11, 0]), 0)
+Test.assertEquals(evenLast([1, 2, 3, 4, 5, 6, 8]), 136)
+Test.assertEquals(evenLast([2, 3, 4, 5]), 30)
+Test.assertEquals(evenLast([2, 4, 6, 8, 9, 11]), 187)
+Test.assertEquals(evenLast([6, 5, 7, 2, 1]), 14)
+Test.assertEquals(evenLast([2, 2, 2, 2]), 8)
+Test.assertEquals(evenLast([5, 1, 2, 3, 4, 6, 7, 8, 4]), 88)
+Test.assertEquals(evenLast([2, 4, 3, 2, 3, 4, 4, 5]), 60)
+Test.assertEquals(evenLast([7, 23, 22, 6, 8, 2]), 74)
+Test.assertEquals(evenLast([33, 2, -22, 5, -6, 5]), 25)
+Test.assertEquals(evenLast([5, 6, 7, 3, 22, 2]), 68)
+Test.assertEquals(evenLast([3, 4, 5, 6, 7, 8]), 120)
+Test.assertEquals(evenLast([1, 4, 5, 6, 7, 2, 3]), 48)
+Test.assertEquals(evenLast([2, 7, 0, 3, 4, 8, 3]), 27)
+Test.assertEquals(evenLast([9, 3, -6, 2, 7, 8]), 80)
+Test.assertEquals(evenLast([7, 7, 7, 7, 1]), 15)
+Test.assertEquals(evenLast([6, 7, 8, 9, 10, 3, 4]), 112)
+Test.assertEquals(evenLast([9, 8, 7, 6, 5, 4, 3, 2]), 48)
+Test.assertEquals(evenLast([]), 0)`;
 
 const funk = testData.match(/(?<=\().*(?=,)/g);
 const ergebnis = testData.match(/(?<=\), ).*(?=\))/g);
@@ -37,11 +36,11 @@ console.log(ergebnis);
 // const ergebnis = testData.match(/(?<=\), ").*(?="\)))/g);
 
 function consolo() {
-  return funk.map((x, i) => `const consolo${i + 1} = ${x}`).join("; <br>");
+  return funk.map((x, i) => `const consolo${i + 1} = ${x}`).join('; <br>');
 }
 
 function displayErgebnis() {
-  return ergebnis.map((x, i) => `const ergebnis${i + 1} = ${x}`).join("; <br>");
+  return ergebnis.map((x, i) => `const ergebnis${i + 1} = ${x}`).join('; <br>');
 }
 
 // console.log(consolo());
@@ -51,9 +50,9 @@ const resultFunk = consolo();
 const resultErgebnis = displayErgebnis();
 
 // Make a div
-const div = document.createElement("div");
+const div = document.createElement('div');
 // add a class of wrapper to it
-div.classList.add("wrapper");
+div.classList.add('wrapper');
 
 // put it into the body
 document.body.appendChild(div);
@@ -83,8 +82,8 @@ function generatePlayerCard(funki, ergebnisse, numm, fun) {
   return html;
 }
 
-const cards = document.createElement("div");
-cards.classList.add("cards");
+const cards = document.createElement('div');
+cards.classList.add('cards');
 
 let cardsHTML = [];
 for (let i = 0; i < funk.length; i++) {
@@ -92,17 +91,17 @@ for (let i = 0; i < funk.length; i++) {
 }
 
 cards.innerHTML = cardsHTML;
-div.insertAdjacentElement("beforebegin", cards);
+div.insertAdjacentElement('beforebegin', cards);
 
-const buttonsDelete = document.querySelectorAll(".delete");
+const buttonsDelete = document.querySelectorAll('.delete');
 // make out delete function
 function deleteCard(event) {
   const buttonThatGotClicked = event.currentTarget;
   // buttonThatGotClicked.parentElement.remove();
-  buttonThatGotClicked.closest(".playerCard").remove();
+  buttonThatGotClicked.closest('.playerCard').remove();
 }
 
 // loop over them and attach a listener
-buttonsDelete.forEach((button) => button.addEventListener("click", deleteCard));
+buttonsDelete.forEach(button => button.addEventListener('click', deleteCard));
 
 // console.log(document.getElementsByClassName('playerCard'));
