dabb1e3369cf407dee77a7adbe717c557c94b57d
ytkyoung <71435307+ytkyoung@users.noreply.github.com>
2021-04-17 23:50:30 +0200

    cardio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,137 @@
-function totalSales(sales, product) {
-  if (!sales[0].join('').includes(product)) {
-    return 'Product not found';
-  }
+const removeRepeats = (str) => str.split(/(.)\1*/g).join('');
 
-  const ind = sales[0].indexOf(product);
-  let sum = 0;
-  for (let i = 1; i < sales.length; i++) {
-    for (let j = 0; j < sales[i].length; j++) {
-      sum += sales[i][ind];
-    }
-  }
-  return sum / sales[0].length;
-}
-
-const result = totalSales(
-  [
-    ['A', 'B', 'C'],
-    [2, 7, 1],
-    [3, 6, 6],
-    [4, 5, 5],
-  ],
-  'B'
-);
-
+const result = removeRepeats('bookkeeper');
 console.log(result);
 
-// function totalSales(sales, product) {
-//   if (!sales[0].join('').includes(product)) {
-//     return 'Product not found';
-//   }
+const testData = `Test.assertEquals(removeRepeats("aaabbbccc"), "abc")
+Test.assertEquals(removeRepeats("bookkeeper"), "bokeper")
+Test.assertEquals(removeRepeats("nananana"), "nananana")
+Test.assertEquals(removeRepeats("accddbccabadcabccdababaacbdaadcccbcaabaaddbabbaadd"), "acdbcabadcabcdababacbdadcbcabadbabad")
+Test.assertEquals(removeRepeats("aabbcabdcddddacdccacbbcabadccbbaadcccbddacbdbabbbd"), "abcabdcdacdcacbcabadcbadcbdacbdbabd")
+Test.assertEquals(removeRepeats("dacbaabacbabacabcabaabdccccbdbbcaadddacdbdbdacbada"), "dacbabacbabacabcababdcbdbcadacdbdbdacbada")
+Test.assertEquals(removeRepeats("cbdbcbcccbdbbcaaaacacbcabddcdcddcccbdaabdacbdcabbd"), "cbdbcbcbdbcacacbcabdcdcdcbdabdacbdcabd")
+Test.assertEquals(removeRepeats("cdbdcdccccbcbbcdabbbbcababccadccabdcacabbcaccdaccd"), "cdbdcdcbcbcdabcababcadcabdcacabcacdacd")
+Test.assertEquals(removeRepeats("bacbdbdadbbbdacbddbdcbadddabbaadcbbdabdaabcdddbacd"), "bacbdbdadbdacbdbdcbadabadcbdabdabcdbacd")
+Test.assertEquals(removeRepeats("daadadccbcacacbacdbbaabaadbaabadacdacadbacdcababbb"), "dadadcbcacacbacdbabadbabadacdacadbacdcabab")
+Test.assertEquals(removeRepeats("bbaccaccbbabbaacbacadadaddbadddbaacbadbcbdaabbbaaa"), "bacacbabacbacadadadbadbacbadbcbdaba")
+Test.assertEquals(removeRepeats("dbdabdccbcdccccbcdbbbcaccdacdacdcacadaddaddbcbdddb"), "dbdabdcbcdcbcdbcacdacdacdcacadadadbcbdb")
+Test.assertEquals(removeRepeats("cdaaaabbccaccacbdabdbbccbbddddddcadcbbccbccdbaddac"), "cdabcacacbdabdbcbdcadcbcbcdbadac")
+Test.assertEquals(removeRepeats("dcccccaacdacbbdaccbabaacddcdddabcadbaaccaabdcabdad"), "dcacdacbdacbabacdcdabcadbacabdcabdad")
+Test.assertEquals(removeRepeats("bcccabddbbacbacacddbbddbbccbdddbcbcabdabdcbccdaddd"), "bcabdbacbacacdbdbcbdbcbcabdabdcbcdad")
+Test.assertEquals(removeRepeats("bcacaabaadcbbdadcddcdaaaddcccdabdbbaddddbabdccabda"), "bcacabadcbdadcdcdadcdabdbadbabdcabda")
+Test.assertEquals(removeRepeats("adacddabacdcbdbadcaacacdcdcdbcdcdabdabbadcbdacbdcd"), "adacdabacdcbdbadcacacdcdcdbcdcdabdabadcbdacbdcd")
+Test.assertEquals(removeRepeats("dbbbbbcddacdbdaacdaaaccbccdccbcbcdabdbddacbcaaadaa"), "dbcdacdbdacdacbcdcbcbcdabdbdacbcada")
+Test.assertEquals(removeRepeats("bdbdbacabcbbacbcaadbcbcbbdaddacbaacabdccadaaddadca"), "bdbdbacabcbacbcadbcbcbdadacbacabdcadadadca")
+Test.assertEquals(removeRepeats("bbabccadacbbbaabddbbcadacddacdcccbacbdcbacbabcabaa"), "babcadacbabdbcadacdacdcbacbdcbacbabcaba")
+Test.assertEquals(removeRepeats("cadaadcbccbcbbdcadbcdcbaacacbcbabbbadaddadcdcacaab"), "cadadcbcbcbdcadbcdcbacacbcbabadadadcdcacab")
+Test.assertEquals(removeRepeats("adbccadacaabbbcdcaabbdbcbdbbcccbcbadcdddcccbbdcdab"), "adbcadacabcdcabdbcbdbcbcbadcdcbdcdab")
+Test.assertEquals(removeRepeats("dbbdddaaadccabbddbbcccbddacccccaabbcdbdbdcdacdbada"), "dbdadcabdbcbdacabcdbdbdcdacdbada")
+Test.assertEquals(removeRepeats("acccdbbbcbdbbbcbaaabcaacacbbcccadddacbbbabbacbccca"), "acdbcbdbcbabcacacbcadacbabacbca")
+Test.assertEquals(removeRepeats("cccbadadcbccacdababbaabdbdcacadabdddcaaaabbaadacca"), "cbadadcbcacdabababdbdcacadabdcabadaca")
+Test.assertEquals(removeRepeats("cdcbdbcadadbbdacbdddaaaddbddcaacdaabcbdccacddacbdd"), "cdcbdbcadadbdacbdadbdcacdabcbdcacdacbd")
+Test.assertEquals(removeRepeats("bdccbadcddabaaccabbdbcdbabdaaddadcbdbaccbacbccdccc"), "bdcbadcdabacabdbcdbabdadadcbdbacbacbcdc")
+Test.assertEquals(removeRepeats("ddacbcadcccaaddbccdbbcdaccabccdaabdbbcddcbbdaccdcd"), "dacbcadcadbcdbcdacabcdabdbcdcbdacdcd")
+Test.assertEquals(removeRepeats("dccbccdbccdcbdcdbadcbbcabbbadbddaaaabdbacaacbdcadd"), "dcbcdbcdcbdcdbadcbcabadbdabdbacacbdcad")
+Test.assertEquals(removeRepeats("accbccdbdaabbbacdcbadcccaccbcbdacaabdddaadbcbaabda"), "acbcdbdabacdcbadcacbcbdacabdadbcbabda")
+Test.assertEquals(removeRepeats("aabcddbcadaacdccbdbcccadcaadcdcbaacdbbaababdbcbabb"), "abcdbcadacdcbdbcadcadcdcbacdbababdbcbab")
+Test.assertEquals(removeRepeats("dccccccccbcacbbabccccbadadbdbdbaaabbccaccdddacbdab"), "dcbcacbabcbadadbdbdbabcacdacbdab")
+Test.assertEquals(removeRepeats("abbddddcdcbcccadbccdcaadddacbddadcccbaacdcdbddccab"), "abdcdcbcadbcdcadacbdadcbacdcdbdcab")
+Test.assertEquals(removeRepeats("dcdacdcbaabcbcbcbacdddabddcabddaaaaadcddcbbabaacbc"), "dcdacdcbabcbcbcbacdabdcabdadcdcbabacbc")
+Test.assertEquals(removeRepeats("cbabcbbbbbdcadcdcadaddadbbbdadbcdabbdddacbbdbcadab"), "cbabcbdcadcdcadadadbdadbcdabdacbdbcadab")
+Test.assertEquals(removeRepeats("aababacdcdcddacadcaddbbccbcbadccdadbbbaddbdaccbcba"), "ababacdcdcdacadcadbcbcbadcdadbadbdacbcba")
+Test.assertEquals(removeRepeats("ddcccbcadbcbbdcdcbbddddcbcdcabcaabdabcbdaddcabcdbd"), "dcbcadbcbdcdcbdcbcdcabcabdabcbdadcabcdbd")
+Test.assertEquals(removeRepeats("dbdadabbccacaaccdaccdbbdbdcacbacadcaabadbccbcbbabc"), "dbdadabcacacdacdbdbdcacbacadcabadbcbcbabc")
+Test.assertEquals(removeRepeats("bbabbddbbadacaadddbddabcabbccddaacbbdbdbccdcdbbdad"), "babdbadacadbdabcabcdacbdbdbcdcdbdad")
+Test.assertEquals(removeRepeats("cbdccccbccbacbcddbabdbabdabcadadbdbcbaddbcddcbdcdc"), "cbdcbcbacbcdbabdbabdabcadadbdbcbadbcdcbdcdc")
+Test.assertEquals(removeRepeats("bdbcacdcddbcadbaadcbcbdbdbdacacdccabbcaadbabbadaac"), "bdbcacdcdbcadbadcbcbdbdbdacacdcabcadbabadac")
+Test.assertEquals(removeRepeats("acaccbcccccaccddbdcbbacacbdccabddacabbdbdcacadddad"), "acacbcacdbdcbacacbdcabdacabdbdcacadad")
+Test.assertEquals(removeRepeats("dcdddddcbccdaaaaadbbbabaabbddbadaadcdcabaabdccaaca"), "dcdcbcdadbababdbadadcdcababdcaca")
+Test.assertEquals(removeRepeats("ddcdbcdcaddcaccbcdcaacbcddacadabacdddcbabdcddabaac"), "dcdbcdcadcacbcdcacbcdacadabacdcbabdcdabac")
+Test.assertEquals(removeRepeats("bccbbaadbcbccddbdbaddaadbbbbbdcdaaddcbcccbbababadc"), "bcbadbcbcdbdbadadbdcdadcbcbababadc")
+Test.assertEquals(removeRepeats("dcdabcdcbbdcbdddbccbcdacbccbbcdbdabbacdbbacacdadca"), "dcdabcdcbdcbdbcbcdacbcbcdbdabacdbacacdadca")
+Test.assertEquals(removeRepeats("dbdbaaacdadbbcbdbcdccdaccaabbccdabbcadbaccbaadbdbb"), "dbdbacdadbcbdbcdcdacabcdabcadbacbadbdb")
+Test.assertEquals(removeRepeats("ccdccbdcabdacdcdcdcbdbaccdcbbbdbaaddbcabbcccbbcaba"), "cdcbdcabdacdcdcdcbdbacdcbdbadbcabcbcaba")
+Test.assertEquals(removeRepeats("bbbccabdcaddcbcadaddaabddaadabcaddcdacbadcadbcadaa"), "bcabdcadcbcadadabdadabcadcdacbadcadbcada")
+Test.assertEquals(removeRepeats("dcadbbccadcbdaaacabacaaadbbdabdabccbcdbccabcbddabb"), "dcadbcadcbdacabacadbdabdabcbcdbcabcbdab")
+Test.assertEquals(removeRepeats("cccdaadabdcabcdababacaddadbddbbcbbcdaaddcdccabbbda"), "cdadabdcabcdababacadadbdbcbcdadcdcabda")
+Test.assertEquals(removeRepeats("aabcabbcdadcccbbdbcbcabdcbacacaabbabcbacbadddccddd"), "abcabcdadcbdbcbcabdcbacacababcbacbadcd")
+Test.assertEquals(removeRepeats("cdbcdacdabdadbddbcbaabadcdccbcccbbacbadccdbccadaba"), "cdbcdacdabdadbdbcbabadcdcbcbacbadcdbcadaba")`;
 
-//   const ind = [];
-//   sales[0].map((x, i) => (x === product ? ind.push(i) : ''));
+const funk = testData.match(/(?<=\().*(?=,)/g);
+const ergebnis = testData.match(/(?<=\), ).*(?=\))/g);
 
-//   console.log(ind);
-//   return [...sales]
-//     .map((x) => x[ind])
-//     .reduce((a, c) => a + (c.toString().match(/\d/g) ? c : ''), '')
-//     .split('')
-//     .reduce((a, c) => +a + +c);
-// }
+console.log(funk);
+console.log(ergebnis);
+// mit String
+// const ergebnis = testData.match(/(?<=\), ").*(?="\)))/g);
+
+function consolo() {
+  return funk.map((x, i) => `const consolo${i + 1} = ${x}`).join('; <br>');
+}
+
+function displayErgebnis() {
+  return ergebnis.map((x, i) => `const ergebnis${i + 1} = ${x}`).join('; <br>');
+}
+
+// console.log(consolo());
+console.log(displayErgebnis());
+
+const resultFunk = consolo();
+const resultErgebnis = displayErgebnis();
+
+// Make a div
+const div = document.createElement('div');
+// add a class of wrapper to it
+div.classList.add('wrapper');
+
+// put it into the body
+document.body.appendChild(div);
+
+// make an unordered list
+const h2Test = `<h2>
+  ${funk[0]}
+</h2><p>${resultFunk} <br><br> ${resultErgebnis}</p>`;
+
+// add three list items with the words "one, two three" in them
+// put that list into the above wrapper
+div.innerHTML = h2Test;
+
+function generatePlayerCard(funki, ergebnisse, numm, fun) {
+  const farbeTrue = `${funki.toString()}` === ergebnisse;
+
+  const html = `
+      <div class="playerCard">
+        <h2>${funki}</h2>
+        <p>${ergebnisse}<p>
+        <p>Test ${numm} ist ${farbeTrue}.</p>
+        <p>${fun}</p>
+        <div><button class="delete" type="button">&times; Delete</button></div>
+      </div>
+    `;
+
+  return html;
+}
+
+const cards = document.createElement('div');
+cards.classList.add('cards');
+
+let cardsHTML = [];
+for (let i = 0; i < funk.length; i++) {
+  cardsHTML += generatePlayerCard(eval(funk[i]), ergebnis[i], i + 1, funk[i]);
+}
+
+cards.innerHTML = cardsHTML;
+div.insertAdjacentElement('beforebegin', cards);
+
+const buttonsDelete = document.querySelectorAll('.delete');
+// make out delete function
+function deleteCard(event) {
+  const buttonThatGotClicked = event.currentTarget;
+  // buttonThatGotClicked.parentElement.remove();
+  buttonThatGotClicked.closest('.playerCard').remove();
+}
+
+// loop over them and attach a listener
+buttonsDelete.forEach((button) => button.addEventListener('click', deleteCard));
+
+// console.log(document.getElementsByClassName('playerCard'));
